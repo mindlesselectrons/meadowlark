44303524ad437b41d44b01aa938fa44305ffc373
tdmreese <tdmreese@gmail.com>
2020-02-28 10:02:52 -0500

    updating project for forms

--- meadowlark.js
+++ meadowlark.js
@@ -5,10 +5,18 @@ var express = require('express')
 var app = express();
 
 //set up handlebars view engine
-var handlebars = require("express3-handlebars").create({defaultLayout:'main'})
+var handlebars = require("express3-handlebars").create({
+  defaultLayout:'main',
+  helpers: {
+    section: function(name, options){
+      if(!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    }
+  }
+})
 //note we specified the default layout (defaultLayout:'main').
 //That means that unless you specify otherwise, this is the layout that will be used for any view
-
 app.engine('handlebars', handlebars.engine)
 app.set('view engine', 'handlebars')
 
@@ -17,6 +25,29 @@ app.set('port', process.env.PORT || 3000)
 app.use(express.static(__dirname + '/public'))
 
 var fortunes = ["Conquer your fears or they will conquer you.","Rivers need springs.","Do not fear what you don't know.","You will have a pleasant surprise.","Whenever possible, keep it simple.", "Apples!"];
+
+
+//dummy weather data middleware
+app.use(function(req,res, next){
+  if(!res.locals.partials) res.locals.partials = {};
+  res.locals.partials.weather = getWeatherData();
+  next();
+})
+
+
+app.get('/nursery-rhyme', function(req, res){
+  res.render('nursery-rhyme');
+});
+
+app.get('/data/nursery-rhyme', function(req, res){
+  res.json({animal: 'squirrel',bodyPart: 'tail',adjective: 'bushy',noun: 'heck',});
+});
+
+
+
+
+
+
 
 //app.get is the method by which we're adding routes
 app.get('/', function(req, res){
@@ -35,6 +66,10 @@ app.get('/apples', function(req, res){
   res.render('aj3')
 })
 
+
+
+
+
 // 404 catch-all handler (middleware)
 app.use(function(req, res){
   res.status(404)
@@ -51,3 +86,32 @@ app.use(function(err, req, res, next){
 app.listen(app.get('port'), function(){
   console.log("express started on localhost:" + app.get('port') + "\nPress ctrl + c to terminate")
 })
+
+//dummy weather data
+function getWeatherData(){
+  return {
+    locations: [
+      {
+        name: 'Portland',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+        weather: 'Overcast',
+        temp: '54.1 F (12.3 C)', //should there be a comma there?
+      },
+      {
+        name: 'Bend',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+        weather: 'Partly Cloudy',
+        temp: '55.0 F (12.8 C)',
+      },
+      {
+        name: 'Manzanita',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+        weather: 'Light Rain',
+        temp: '55.0 F (12.8 C)',
+      },
+    ],
+  };//end of return
+}//end of getWeatherData()
